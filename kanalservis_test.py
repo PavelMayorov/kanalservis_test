7be845e5116396bac243d82e9aac708a33988b93
Майоров П.В <mayorov.pv@psma.edu>
2022-07-04 16:30:48 +0500

    exception handling refactoring

--- kanalservis_test.py
+++ kanalservis_test.py
@@ -15,15 +15,15 @@ def get_usd_exchange_rate() -> float:
     """Получает курс USD с сайта ЦБ"""
     try:
         response = requests.get(url=URL)
+    except Exception as ex:
+        logger.error(f'Нет соединения с сайтом ЦБ РФ!\n{ex}')
+    else:
         root = ET.fromstring(response.text)
         for valute in root:
             if valute.find('CharCode').text == 'USD':
                 rate = float(valute.find('Value').text.replace(',', '.'))
                 logger.info('Курс доллара получен!')
                 return rate
-    except Exception as ex:
-        logger.error('Нет соединения с сайтом ЦБ РФ!')
-        logger.error(ex)
 
 
 def get_data_spreadsheet() -> list:
@@ -33,51 +33,57 @@ def get_data_spreadsheet() -> list:
     http_auth = credentials.authorize(httplib2.Http())
     try:
         service = build('sheets', 'v4', http=http_auth)
+    except Exception as ex:
+        logger.error(f'Нет соединения с сайтом Google Sheets!\n{ex}')
+    else:
         # Чтение файла по 10 строк
         values, i = [], 1
         while True:
-            value = service.spreadsheets().values().get(
-                spreadsheetId=SPREADSHEET_ID,
-                range=f'A{i}:D{i+9}',
-                majorDimension='ROWS'
-            ).execute()
-            if value.get('values'):
-                values += value['values']
-                i += 10
+            try:
+                value = service.spreadsheets().values().get(
+                    spreadsheetId=SPREADSHEET_ID,
+                    range=f'A{i}:D{i+9}',
+                    majorDimension='ROWS'
+                ).execute()
+            except Exception as exc:
+                logger.error(f'Нет соединения с сайтом Google Sheets!\n{exc}')
+                break
             else:
-                logger.info('Данные из Google таблицы получены!')
-                return values
-    except Exception as ex:
-        logger.error('Нет соединения с сайтом Google Sheets!')
-        logger.error(ex)
+                if value.get('values'):
+                    values += value['values']
+                    i += 10
+                else:
+                    logger.info('Данные из Google таблицы получены!')
+                    return values
 
 
 def create_dataframe(values: list, rate: float):
     """Создает dataframe из 'values' с добавлением колонки 'стоимость,Р' на основе курса USD (rate)"""
     try:
         df = pd.DataFrame(values[1:], columns=values[0]).set_index('№')
-        try:
-            df = df.astype({'стоимость,$': 'int'})
-            df['стоимость,Р'] = round(df['стоимость,$'] * rate, 2)
-            return df
-        except (TypeError, ValueError) as ex:
-            logger.error('Столбец "стоимость,$" заполнен не корректно!')
-            logger.error(ex)
     except ValueError:
         logger.error('Один из столбцов пуст!')
+    else:
+        try:
+            df = df.astype({'стоимость,$': 'int'})
+        except (TypeError, ValueError) as ex:
+            logger.error(f'Столбец "стоимость,$" заполнен не корректно!\n{ex}')
+        else:
+            df['стоимость,Р'] = round(df['стоимость,$'] * rate, 2)
+            return df
 
 
 def save_to_db(df):
     """Сохраняет dataframe 'df' в БД на основе СУБД PostgreSQL"""
     try:
         engine = create_engine(f'postgresql+psycopg2://{USER}:{PASSWORD}@{HOST}:{PORT}/test')
+    except exc.OperationalError as ex:
+        logger.error(f'Не удалось подключится к базе данных!\n{ex}')
+    else:
         if not database_exists(engine.url):
             create_database(engine.url)
         df.to_sql('test_table', con=engine, if_exists='replace')
         logger.info('Данные сохранены в БД!')
-    except exc.OperationalError as ex:
-        logger.error('Не удалось подключится к базе данных!')
-        logger.error(ex)
 
 
 def main():
